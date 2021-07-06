33acf2427260a3721363697a686f8ca78fe74a7d
Steve Creekmore <Creekmore108@gmail.com>
2021-07-06 17:46:11 -0600

    bg colors

--- resources/views/layouts/features.blade.php
+++ resources/views/layouts/features.blade.php
@@ -29,7 +29,7 @@
 
           <div>
             <div>
-              <span class="flex items-center justify-center h-12 w-12 rounded-md bg-gray-900">
+              <span class="flex items-center justify-center h-12 w-12 rounded-md bg-gray-600">
                 <!-- Heroicon name: outline/users -->
                 <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
@@ -46,7 +46,7 @@
 
           <div>
             <div>
-              <span class="flex items-center justify-center h-12 w-12 rounded-md bg-gray-800">
+              <span class="flex items-center justify-center h-12 w-12 rounded-md bg-gray-600">
                  <!-- Heroicon name: outline/clipboard list -->
                  <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                 <path d="M9 5H7C5.89543 5 5 5.89543 5 7V19C5 20.1046 5.89543 21 7 21H17C18.1046 21 19 20.1046 19 19V7C19 5.89543 18.1046 5 17 5H15M9 5C9 6.10457 9.89543 7 11 7H13C14.1046 7 15 6.10457 15 5M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5M12 12H15M12 16H15M9 12H9.01M9 16H9.01" stroke="dark-yellow-600" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
