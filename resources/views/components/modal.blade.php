74f8b8f66fbc3d3043018f4186e6cb3483fbcb76
Steve Creekmore <Creekmore108@gmail.com>
2021-07-07 16:43:56 -0600

    commented out HREF in Modal.blade.php

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -13,11 +13,11 @@
     <div x-show="show" class="rounded-lg mt-16 bg-yellow-600 overflow-hidden transform sm:w-full sm:mx-auto max-w-lg">
         
         <!-- <div class="absolute top-0 right-0 mr-2 mt-2" > -->
-        <a  href="/?#footer"   x-data="{}" x-on:click="window.livewire.emitTo('contact-modal', 'hide')">
+        <!-- <a  href="/?#footer"   x-data="{}" x-on:click="window.livewire.emitTo('contact-modal', 'hide')"> -->
          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mt-1 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
-        </a>
+        <!-- </a> -->
         <!-- </div> -->
         {{ $slot }}
     
